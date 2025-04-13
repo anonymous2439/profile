ad42631b7db63ffc85704c2c674a6ffeb589b99c
anonymous <mranonymous2439@gmail.com>
2025-04-13 21:42:13 +0800

    modification

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
     <div>
 
         <div class="flex justify-center mb-[40px] flex-col items-center text-center sm:flex-row sm:text-left gap-[20px]">
-            <figure class="relative bg-red-500 w-[250px] h-[300px] overflow-hidden rounded-[20px] min-w-[250px]">
-                <img class="profile-img" src="/images/profile2.jpg" />
+            <figure class="relative w-[250px] h-[300px] overflow-hidden rounded-[20px] min-w-[250px]">
+                <img class="h-auto w-[319px] max-w-[1080px] absolute left-1/2 -translate-x-1/2 saturate-[0.9] brightness-[0.9]" 
+                    src="/images/profile2.jpg" />
             </figure>
             <h2 class="max-w-[300px] text-4xl/13 flex justify-center flex-col">
                 <span class="block">Hi, I'm <b>Neil</b>,</span> An Experienced Web Developer
@@ -28,13 +29,5 @@
 </script>
 
 <style lang="css" scoped>
-    .profile-img {
-        height: auto;
-        width: 319px;
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-        max-width: 1080px;
-        filter: saturate(0.9) brightness(0.9);
-    }
+    
 </style>
